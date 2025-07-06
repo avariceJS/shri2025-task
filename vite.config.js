377d36b7e3ef70f9440da4ad9932832a23438d64
avaricejs <avaricejs@mail.ru>
2025-07-06 23:15:31 +0300

    Performance task

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  publicDir: 'assets',
+  assetsInclude: ['**/*.woff2', '**/*.svg', '**/*.png', '**/*.webp']
+});
